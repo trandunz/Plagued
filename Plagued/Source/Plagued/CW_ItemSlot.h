41f16c131e264a34eb74c8df326c41d4e0dad9e0
trandunz <54924605+trandunz@users.noreply.github.com>
2022-12-11 21:57:48 +1300

    + CW_StorageContainer

---
+++ Plagued/Source/Plagued/CW_ItemSlot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CW_ItemSlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAGUED_API UCW_ItemSlot : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
